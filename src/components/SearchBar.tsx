2b0b073cd1a995e6c835c09aa12b4024b5102627
Firstname Lastname <you@example.com>
2020-12-31 10:25:40 -0800

    Basic search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,19 +1,39 @@
-import '../css/search-bar.css';
-import React from 'react';
-import MenuButton from './MenuButton';
-import {
-  Link
-} from "react-router-dom";
+import "../css/search-bar.css";
+import React, { useState } from "react";
+import CloseButton from "./CloseButton";
 
 export default function () {
-    return (
-        <header className="SearchBar">
-            <div className="SearchBar-wrapper">
-                <MenuButton />
-                <span className="SearchLink"><Link to="/search">Search station</Link></span>
-            </div>
-        </header>
-    );
+  const [searchStr, setSearchStr] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchStr(e.target.value);
+  };
+
+  const handleInputEnterKey = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      console.log("ENTER", e);
+    }
+  };
+
+  const handleClickClose = () => {
+    setSearchStr("");
+  };
+
+  return (
+    <header className="SearchBar">
+      <div className="SearchBar-wrapper">
+        <input
+          className="SearchInput"
+          onChange={handleInputChange}
+          onKeyDown={handleInputEnterKey}
+          placeholder="Search stations"
+          type="text"
+          value={searchStr}
+        />
+        <CloseButton onClick={handleClickClose} />
+      </div>
+    </header>
+  );
 }
 
 // TODO: Store 5 most recent searches to show in suggestions
--- src/components/StationCard.tsx
+++ src/components/StationCard.tsx
@@ -1,34 +1,44 @@
-import '../css/station-card.css';
-import React from 'react';
-import {
-  Link
-} from "react-router-dom";
+import "../css/station-card.css";
+import React from "react";
+// import {
+//   Link
+// } from "react-router-dom";
 
 export type StationType = {
-    name: string,
-    abbr: string,
-    gtfs_latitude: number,
-    gtfs_longitude: number,
-    address: string,
-    city: string,
-    county: string,
-    state: string,
-    zipcode: number
+  name: string;
+  abbr: string;
+  gtfs_latitude: number;
+  gtfs_longitude: number;
+  address: string;
+  city: string;
+  county: string;
+  state: string;
+  zipcode: number;
 };
 
 type Props = {
-    station: StationType
-}
+  station: StationType;
+};
 
 export default function ({ station }: Props) {
-    return (
-        <article className="StationCard">
-            <Link to={`/station/${station.abbr}`}>
-                <header>
-                    <h2>{station.name}</h2>
-                </header>
-            <p>{station.address}, {station.city}</p>
-            </Link>
-        </article>
-    )
+  return (
+    <article className="StationCard">
+      <header>
+        <h2>{station.name}</h2>
+      </header>
+      <p>
+        {station.address}, {station.city}
+      </p>
+    </article>
+  );
+  // return (
+  //     <article className="StationCard">
+  //         <Link to={`/station/${station.abbr}`}>
+  //             <header>
+  //                 <h2>{station.name}</h2>
+  //             </header>
+  //         <p>{station.address}, {station.city}</p>
+  //         </Link>
+  //     </article>
+  // )
 }
