14242d190691dfd91cc1b553038b2cd6cac6b194
Firstname Lastname <you@example.com>
2020-12-30 18:32:35 -0800

    Fix

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export type StationType = {
+  name: string;
+  abbr: string;
+  gtfs_latitude: number;
+  gtfs_longitude: number;
+  address: string;
+  city: string;
+  county: string;
+  state: string;
+  zipcode: number;
+};
