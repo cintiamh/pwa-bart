3f7e3b7da64fd41a702fdc52d98dcad409517a21
Firstname Lastname <you@example.com>
2021-01-02 10:06:00 -0800

    More clean up

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import MenuIcon from './MenuIcon';
+import React from "react";
+import MenuIcon from "./MenuIcon";
 
-export default function () {
-    return (<button onClick={ () => { console.log('Show menu')} }><MenuIcon /></button>);
+export default function MenuButton() {
+  return (
+    <button
+      onClick={() => {
+        console.log("Show menu");
+      }}
+    >
+      <MenuIcon />
+    </button>
+  );
 }
 
 // TODO: items: All stations, Closest, Recent (order by most recent), Starred
--- src/components/MenuIcon.tsx
+++ src/components/MenuIcon.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
 
-export default function () {
-    return (
-        <span className="MenuIcon">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="#EFF8FF" height="24" viewBox="0 0 24 24" width="24">
-                <path d="M0 0h24v24H0V0z" fill="none" />
-                <path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z" />
-            </svg>
-        </span>
-    );
+export default function MenuIcon() {
+  return (
+    <span className="MenuIcon">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="#EFF8FF"
+        height="24"
+        viewBox="0 0 24 24"
+        width="24"
+      >
+        <path d="M0 0h24v24H0V0z" fill="none" />
+        <path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z" />
+      </svg>
+    </span>
+  );
 }
